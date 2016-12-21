557448fa435e7be95c53c12b22f3a2d857953795
Tony Ross-Hellauer <ajcross01@gmail.com>
2016-12-21 18:27:44 -0500

    added file wtid.js

---
+++ wtid.js
@@ -0,0 +1,3 @@
+if (typeof(Webtrends.dcss.dcsobj_0.dcsGetIdCallback)=="function"){
+Webtrends.dcss.dcsobj_0.dcsGetIdCallback({"gWtId":"","gTempWtId":"a819bd16-c051-4d09-94da-4a6e0378c226","gWtAccountRollup":"1"});
+}
