51b240fc171032b3ad69d35a008d0bb8d53b4e79
Tony Ross-Hellauer <ajcross01@gmail.com>
2016-12-21 18:27:32 -0500

    added file serverComponent.php

---
+++ serverComponent.php
@@ -0,0 +1,2 @@
+
+Bootstrapper._serverTime = '2015-10-19 09:13:38'; Bootstrapper._clientIP = '134.76.162.165'; var psj0 = 'http://nexus.ensighten.com/npg/prod-sa/code/e68efaec1633db151263489583b5e6a1.js?conditionId0=203579';Bootstrapper.insertScript(psj0);var psj1 = 'http://nexus.ensighten.com/npg/prod-sa/code/fa3d20a615111fde0e1a70e4f38f3be8.js?conditionId0=203628';Bootstrapper.loadScriptCallback(psj1, Bootstrapper.callOnPageSpecificCompletion);Bootstrapper.setPageSpecificDataDefinitionIds([])
